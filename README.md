3330d20a43e80619c8a4cda5ddaa42da114e7fb0
Alejandro <alviti07@gmail.com>
2025-10-15 06:15:17 -0500

    despliegue commit

--- README.md
+++ README.md
@@ -18,7 +18,13 @@ npm i -g @nestjs/cli
 ```
 docker-compose up -d
 ```
-5. Reconstruir la base de datos con la semilla 
+5. Clonar el archivo __```.env.template```__ y renombrar la copia __```.env```__
+6. Llenar las variables de entorno definidas en el __```.env```__
+7. Ejecutar la aplicacion em dev:
+```
+npm run start:dev
+```
+8. Reconstruir la base de datos con la semilla 
 ```
 http://localhost:3000/api/v2/seed
 ```
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,19 +1,29 @@
 import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { isValidObjectId, Model } from 'mongoose';
-import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { PaginationDto } from '../common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit : number;
+
   constructor(
 
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
 
-  ) {}
+    private readonly configService: ConfigService
+
+  ) {
+
+    this.defaultLimit = configService.get<number>('defaultLimit') || 5 ;
+
+  }
 
 
   async create(createPokemonDto: CreatePokemonDto) {
@@ -31,8 +41,17 @@ export class PokemonService {
 
   }
 
-  findAll() {
-    return `This action returns all pokemon`;
+  findAll( paginationDto: PaginationDto ) {
+
+    const {limit = this.defaultLimit, offset= 0} = paginationDto;
+
+    return this.pokemonModel.find()
+      .limit(limit)
+      .skip(offset)
+      .sort({
+        no: 1
+      })
+      .select('-__v'); //quita de la consulta
   }
 
   async findOne(term: string) {
